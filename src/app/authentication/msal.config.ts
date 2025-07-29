6e0787b3d82adc33011244456efeb7bceeea992b
JuanGMejia <JuanGMejia@users.noreply.github.com>
2025-07-29 08:03:23 -0500

    Update msal.config.ts

--- src/app/authentication/msal.config.ts
+++ src/app/authentication/msal.config.ts
@@ -7,7 +7,7 @@ export function AuthFactory(): PublicClientApplication {
   return new PublicClientApplication({
     auth: {
       clientId: CLIENT_ID, // from Azure Portal
-      redirectUri: 'https://test-car-ecommerce-fe-eddhaefjawbnhsa6.chilecentral-01.azurewebsites.net',
+      redirectUri: 'car-ecommerce-ui-bvewdmhzdng6araf.canadacentral-01.azurewebsites.net',
       authority: `https://login.microsoftonline.com/${TENANT_ID}` // or your tenant ID
     },
     cache: {
