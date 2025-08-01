3e8bb03910c521c748e36d3023a73e055f6ad1d6
Juan Guillermo Mejia Soto <juan.mejias@perficient.com>
2025-08-01 17:00:22 -0500

    Fix integration tests

--- src/assets/runtime-config.js
+++ src/assets/runtime-config.js
@@ -1,4 +1,4 @@
 window.__env = {
-  CLIENT_ID: "REPLACEME_API_URL",
-  TENANT_ID: "REPLACEME_KEY"
+  CLIENT_ID: "9984aef7-9bb3-4cd3-ae70-6251b4b188b8",
+  TENANT_ID: "a747b20f-59c5-4814-9ae9-40b871d08ff6"
 };
